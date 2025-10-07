2d7f41ea6a6f47cfe229916f5ef7b9aec3b16cf1
Smols <carterd@vandenrobotics.com>
2025-10-07 16:30:20 -0700

    Added States and Enums
    I love coding so much :DDD

--- carter-and-caleb/src/main/java/frc/robot/subsystems/AwesomeSauceSubsystem.java
+++ carter-and-caleb/src/main/java/frc/robot/subsystems/AwesomeSauceSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Inches;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,14 +17,23 @@ public class AwesomeSauceSubsystem extends SubsystemBase {
 
   public String weirdSauceString ="69";
 
+  public enum LauncherState {
+  S_empty, S_filled,S_launching
 
+  }
+  public static LauncherState mLauncherState;
   
   public AwesomeSauceSubsystem() {
     AwesomeSauceMotor = new TalonFX(1);
+    mLauncherState = LauncherState.S_empty;
   }
 
   public void spinAwesomeSauceMotor() {
     AwesomeSauceMotor.setVoltage(5);
+  }
+
+  public void LaunchAwesomeSauceMotor() {
+    AwesomeSauceMotor.setVoltage(15);
   }
 
 public void stopAwesomeSauceMotor() {
@@ -33,10 +44,23 @@ public void MoveAwesomeSauce(double speed) {
   AwesomeSauceMotor.setVoltage(speed* 10);
 }
 
+public void RunLauncherState() {
+switch (mLauncherState) {
+  case S_empty:
+    spinAwesomeSauceMotor();
+    break;
+    case S_filled:
+    stopAwesomeSauceMotor();
+    break;
+    case S_launching:
+    LaunchAwesomeSauceMotor();
+    break;
+  }
+}
 
   @Override
   public void periodic() {
-    AwesomeSauceMotor.setVoltage(5);
+    RunLauncherState();
     SmartDashboard.putString("weirdSauceString", weirdSauceString);
   }
 
